1b1d0f4168da64424f6bac2dfdf674e4d6eb9153
Aki Rodic <aleksandar.xyz@gmail.com>
2022-05-28 12:55:17 +0200

    Added words

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ IoThemeSingleton.theme = JSON.parse(localStorage.getItem('darkTheme') || 'false'
 const today = Math.floor((Number(new Date()) + 1000 * 60 * 60 * 2) / (1000 * 60 * 60 * 24));
 // Get word of the day
 const answer = getWordOfTheDay(today);
-
+console.log(answer);
 // Board state. Each tile is represented as { letter, state }
 const board = history.load(today) || Array.from({ length: 6 }, () =>
 Array.from({ length: 5 }, () => ({
